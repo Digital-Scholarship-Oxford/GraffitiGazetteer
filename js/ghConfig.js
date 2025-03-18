16fd37c0208aa7561a2887d9bd036e99e87cdeb0
Imran Asif <56230659+imranasif87@users.noreply.github.com>
2025-03-18 17:00:11 +0000

    Add files via upload

---
+++ js/ghConfig.js
@@ -0,0 +1,10 @@
+
+
+ // GitHub repository details
+ const owner = 'imranasif87'; // Replace with your GitHub username
+ const repo = 'GraffitiGazetteer'; // Replace with your repository name
+ const path = 'data/graffiti.csv'; // The path inside your repo
+ const organization = 'Digital-Scholarship-Oxford'; // Your organization name
+
+
+ var apiURL = `https://api.github.com/repos/${organization}/${repo}/contents/${path}`;
